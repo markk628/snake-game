d61ac682efadb1d1589d1df9ce2769fdfab232c9
Donghyun <markk628@gmail.com>
2019-11-17 16:32:44 -0800

    restarted code

--- snake.py
+++ snake.py
@@ -1,16 +1,10 @@
-import math
-import random
-import pygame
-import tkinter as tk
-from tkinter import messagebox
+import turtle
 
-class Cube(object):
-    rows = 0
-    w = 0
-    def __init__(self, start, dirnx=1, dirnx=0, color=(255,0,0)):
-        pass
-    def move(self, dirnx, dirny):
-        pass
-    def draw(self, surface, eyes=False):
-        pass
-    
+window = turtle.Screen()
+window.title("Snake Game")
+window.bgcolor("black")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+
+window.mainloop()
