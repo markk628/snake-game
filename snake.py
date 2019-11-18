0edf937c4e6b695308280ecc600aa888432c2fb6
Donghyun <markk628@gmail.com>
2019-11-17 16:25:22 -0800

    first commit

---
+++ snake.py
@@ -0,0 +1,16 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class Cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirnx=0, color=(255,0,0)):
+        pass
+    def move(self, dirnx, dirny):
+        pass
+    def draw(self, surface, eyes=False):
+        pass
+    
