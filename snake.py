53eb18fd83d2174dfe14b058d04ca52c276899f4
Donghyun <markk628@gmail.com>
2019-11-17 20:28:22 -0800

    added head direction and game loop

--- snake.py
+++ snake.py
@@ -1,10 +1,63 @@
 import turtle
+import time
 
+delay = 0.1
+
+#game screen
 window = turtle.Screen()
 window.title("Snake Game")
 window.bgcolor("black")
 window.setup(width=600, height=600)
 window.tracer(0)
 
+#snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "right"
+
+#functions
+def up():
+    head.direction == "up"
+
+def down():
+    head.direction == "down"
+
+def right():
+    head.direction == "right"
+
+def left():
+    head.direction == "left"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 15)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 15)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 15)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x + 15)
+
+#keyboard bindings
+window.listen()
+
+#game main loop
+while True:
+    window.update()
+
+    move()
+
+    time.sleep(delay)
 
 window.mainloop()
