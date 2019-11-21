3d5492371067a90a485c19c7e13945b47bfb98d6
Donghyun <markk628@gmail.com>
2019-11-20 17:09:09 -0800

    added snake body, food, collisions

--- snake.py
+++ snake.py
@@ -1,14 +1,15 @@
 import turtle
 import time
+import random
 
 delay = 0.1
 
 #game screen
-window = turtle.Screen()
-window.title("Snake Game")
-window.bgcolor("black")
-window.setup(width=600, height=600)
-window.tracer(0)
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
 
 #snake head
 head = turtle.Turtle()
@@ -17,20 +18,34 @@ head.shape("square")
 head.color("white")
 head.penup()
 head.goto(0,0)
-head.direction = "right"
+head.direction = "stop"
+
+#snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("blue")
+food.penup()
+food.goto(0,100)
+
+segments = []
 
 #functions
-def up():
-    head.direction == "up"
+def go_up():
+    if head.direction != "down":
+        head.direction == "up"
 
-def down():
-    head.direction == "down"
+def go_down():
+    if head.direction != "up":
+        head.direction == "down"
 
-def right():
-    head.direction == "right"
+def go_right():
+    if head.direction != "left":
+        head.direction == "right"
 
-def left():
-    head.direction == "left"
+def go_left():
+    if head.direction != "right":
+        head.direction == "left"
 
 def move():
     if head.direction == "up":
@@ -47,17 +62,67 @@ def move():
 
     if head.direction == "left":
         x = head.xcor()
-        head.setx(x + 15)
+        head.setx(x - 15)
 
 #keyboard bindings
-window.listen()
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_right, "d")
+wn.onkeypress(go_left, "a")
+
 
 #game main loop
 while True:
-    window.update()
+    wn.update()
+
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        segments.clear()
+
+
+    if head.distance(food) < 20:
+        x = random.randint(-290,290)
+        y = random.randint(-290,290)
+        food.goto(x,y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("circle")
+        new_segment.color("red")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    for index in range(len(segments)-1,0,-1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
 
     move()
 
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction("stop")
+        
+            for segment in segments:
+                segment.goto(1000,1000)
+
+            segments.clear()
+
     time.sleep(delay)
 
-window.mainloop()
+wn.mainloop()
